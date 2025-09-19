1a698d1875f4fa713283ae4daa54af69a3d59e92
Projectwebservice <poowanatrakying@gmail.com>
2025-09-19 11:08:34 +0700

    update1

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -19,5 +19,5 @@ test('get started link', async ({ page }) => {
 });
 
 test('facebook', async ({ page }) => {
-  await page.goto('https//www.facebook.com');
+  await page.goto('https://www.facebook.com');
 });
